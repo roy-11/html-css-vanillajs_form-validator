bf15474e2ca7a81f977bf36484f2e5ded9afadb6
ryosukehagiwara <hukurahagi1109@gmail.com>
2020-04-30 16:20:22 +0900

    add checking length & email & password match

--- script.js
+++ script.js
@@ -7,6 +7,10 @@ const password2 = document.getElementById("password2");
 form.addEventListener("submit", (e) => {
   e.preventDefault();
   checkRequired([username, email, password, password2]);
+  checkLength(username, 3, 15);
+  checkLength(password, 6, 25);
+  checkEmail(email);
+  checkPasswordMatch(password, password2);
 });
 
 function showError(input, message) {
@@ -21,9 +25,12 @@ function showSuccess(input) {
   formControl.className = "form-control success";
 }
 
-function isValidEmail(email) {
-  var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  return re.test(String(email).toLowerCase());
+function checkEmail(email) {
+  const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  const isEmail = re.test(String(email.value.trim()).toLowerCase());
+  console.log(isEmail);
+  if (isEmail) showSuccess(email);
+  else showError(email, "E-mail is not valid");
 }
 
 function checkRequired(inputs) {
@@ -34,5 +41,20 @@ function checkRequired(inputs) {
 }
 
 function getFieldName(input) {
-  return input.id[0].toUpperCase() + input.id.slice(1);
+  if (input.id !== "password2") return input.id[0].toUpperCase() + input.id.slice(1);
+  return "Confirmation";
+}
+
+function checkLength(input, min, max) {
+  if (input.value.length < min) {
+    showError(input, `${getFieldName(input)} must be at least ${min} characters`);
+  } else if (input.value.length > max) {
+    showError(input, `${getFieldName(input)} must be less than ${max} characters`);
+  } else {
+    showSuccess(input);
+  }
+}
+
+function checkPasswordMatch(input1, input2) {
+  if (input1.value !== input2.value) showError(password2, "Passwords do not match");
 }
